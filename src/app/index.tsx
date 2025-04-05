0fcc78745271e55f62d624681b0aac3c833df3eb
unknown <jp1.7felix@gmail.com>
2025-04-05 17:21:43 -0300

    adicionando estilização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,42 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function Index () {
     return ( 
         // Componente Text sendo usando para Renderizar um texto na tela
        <View>
-        <Text> eu AAAAAAAAAAAAAg </Text>
-       <Text>João Pedro</Text>
-       <Text> Pokémon </Text>
-       <Text> Minecreft </Text>
+
+        <Text style={style.title} > eu AAAAAAAAAAAAAg </Text>
+
+       <Text style={style.title2}>João Pedro</Text>
+       <Text style={style.title3}> Pokémon </Text>
+       <Text style={style.title4}> Minecreft </Text>
        </View>
     
 
     
     );
 }
+
+const style = StyleSheet.create({
+    title:{
+        color:"red",
+        fontSize: 22,
+
+    },
+    title2: {
+        color: "blue",
+        fontSize:26,
+    },
+
+
+     title3: {
+     color: "blue",
+     fontSize:30,
+
+},
+title4: {
+    color: "blue",
+    fontSize:40,
+},
+
+})
